c537fb6385f92c74e1c7a9fb675fa7b2046920c4
Exalted <exaltedlord13@gmail.com>
2026-04-09 11:59:12 +0800

    vercel allow

--- backend/utils/env.js
+++ backend/utils/env.js
@@ -53,7 +53,7 @@ function getConfig() {
     allowedOrigins: new Set(
       (process.env.FRONTEND_ORIGINS ||
         [
-          "https://toto-dental.vercel.app",
+          "https://toto-dental-hjbv.vercel.app/",
           "http://127.0.0.1:3000",
           "http://localhost:3000",
           "http://127.0.0.1:5173",
